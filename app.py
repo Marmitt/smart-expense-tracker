cea512d1af241350ec14590a17692c2601c88fa1
Mateus Marmitt <lmateus.marmitt@gmail.com>
2025-04-09 10:31:05 +1000

    Final Adjustments

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
+    password = db.Column(db.String(256), nullable=False)
 
 class Income(db.Model):
     id = db.Column(db.Integer, primary_key=True)
